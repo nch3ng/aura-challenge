7b45ed5405a16edb882fe1b8864969d0c85fdfc9
Nate Cheng <nate.cheng@parcelpending.com>
2020-12-07 08:08:20 -0800

    Move the parameters reader in front of the DB connect, and add yup library

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
 import * as util from "./utils";
 import DataStore from "./DataStore";
 import { generateGrid } from "./import";
+import * as yup from 'yup';
+// import * as joi from 'joi';
+
 
 const handler = async (event: any): Promise<any> => {
   // console.log("Received event:", JSON.stringify(event, null, 2));
-  let result;
+  let result: any;
+  // const querySchema = yup.object({
+  //   city: yup.string(),
+  //   zipcode: yup.string(),
+  //   coordinate: yup.string()
+  // });
+  // const validated: any = querySchema.validateSync(event.queryStringParameters);
+  // const valid = await querySchema.isValid(event.queryStringParameters);
+  // console.log(valid);
   try {
+    
+    const param: any = util.readParams(event);
+    if (!param) throw "400 Invalid Input";
+
     const ds: DataStore = new DataStore(
       process.env.ZIPS_DATA_PATH || "",
       process.env.GRID_INDEX_DATA_PATH || ""
     );
     ds.connect();
-    const param: any = util.readParams(event);
 
-    if (!param) throw "400 Invalid Input";
+    
 
     result = param.name !== "coordinate" ? ds.search(param) : ds.findClosestZip(param.value);
 
